f79c60b0aabcf0e4634bc584c40521f5e5a87590
Grant Gabbert <grantok@gmail.com>
2021-08-28 18:42:04 -0500

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // paths
 const pubDir = path.join(__dirname, '../public')
@@ -98,4 +99,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Started on ${port}`)
+})
